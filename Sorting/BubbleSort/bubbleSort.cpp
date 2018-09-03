0912d15084b419e6c93cdcdda2b18c4272e24fbf
Jack Rosetti <32694046+jackrosetti@users.noreply.github.com>
2018-09-03 15:34:07 -0400

    Refactor methods

--- Sorting/BubbleSort/bubbleSort.cpp
+++ Sorting/BubbleSort/bubbleSort.cpp
@@ -3,43 +3,48 @@
 
 using namespace std;
 
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++){      
+       for (j = 0; j < n-i-1; j++){ 
+           if (arr[j] > arr[j+1]){
+              swap(&arr[j], &arr[j+1]);
+           }
+       }
+   }
+ 	for(i=0;i<n;i++)
+ 		cout<<arr[i]<<", ";
+    	
+    cout<<"\n";
+}
+
 int main()
 {
 
-	clock_t t1,t2;
+    clock_t t1,t2;
     t1 = clock();
 
-	int n;
-	cout<<"How many numbers in the array? \n";
-	cin>>n;
-	int Array[n];
-	cout<<"Enter " << n << " numbers for the array";
-	//take input
-	for(int i = 0; i < n; i++){
-		cin>>Array[i];
-	}
+    int arr[100],n;
+    cout<<"Enter the size of array : \n";
+    cin>>n;
+    cout<<"Enter the elements : \n";
+    for(int i=0 ; i<n ; i++){
+        cin>>arr[i];
+        }
+    //int n = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, n);
 
-	//sort
-	for(int i = 0; i < n; i++){
-		for(int j = 0; j < n-1; j++){
-			if(Array[j] > Array[j + 1]){
-				int temp = Array[j];
-				Array[j] = Array[j+1];
-				Array[j + 1] = temp; 
-			}
-		}
-	}
-
-	//print
-	cout<<"Sorted array is: ";
-	for(int i = 0; i < n; i++){
-		cout<<Array[i]<<"\t";
-	}
-
-	t2 = clock();
-    float diff ((float)t2-(float)t1);
-    float secs = diff/1000;
-    cout<<"\nTIME TAKEN:" <<secs<<endl;
-
-
+    t2 = clock();
+    float diff (((float)t2-(float)t1)/1000);
+    cout<<"\nTIME TAKEN: " <<diff<<endl;
+    
+    return 0;
 }
